5691a1299de8a22cfcff70ad37ff0a84a24ccbef
Sagar Patel <srp324@drexel.edu>
2018-04-08 01:58:35 -0400

    Setup nodejs server with mongodb

---
+++ server/routes/api.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+// Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost', function (err, client) {
+        if (err) throw err;
+      
+        var db = client.db('mean');
+      
+        db.collection('users').findOne({}, function (findErr, result) {
+          if (findErr) throw findErr;
+          console.log(result.name);
+          client.close();
+        });
+
+        closure(db);
+      }); 
+};
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+// Get users
+router.get('/users', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .find()
+            .toArray()
+            .then((users) => {
+                response.data = users;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
+
+module.exports = router;
